3d425ac12171c08de7211ff231e82df8b39edd69
skrusche63 <skrusche63@googlemail.com>
2014-11-30 15:27:07 +0100

    Indexer functionality moved to 'spark-core'.

--- src/main/scala/de/kp/spark/arules/actor/RuleIndexer.scala
+++ src/main/scala/de/kp/spark/arules/actor/RuleIndexer.scala
@@ -18,78 +18,11 @@ package de.kp.spark.arules.actor
  * If not, see <http://www.gnu.org/licenses/>.
  */
 
-import de.kp.spark.core.model._
-import de.kp.spark.core.elastic.{ElasticBuilderFactory => EBF}
-
-import de.kp.spark.core.io.ElasticIndexer
-
-import de.kp.spark.arules.model._
+import de.kp.spark.core.actor.BaseIndexer
+import de.kp.spark.arules.Configuration
 
 /**
  * RuleIndexer supports the administration task of creating an
- * Elasticsearch index, both for the registration of purchase
- * items and association rules
+ * Elasticsearch index.
  */
-class RuleIndexer extends BaseActor {
-  
-  def receive = {
-    
-    case req:ServiceRequest => {
-
-      val uid = req.data("uid")
-      val origin = sender
-
-      try {
-
-        val index   = req.data("index")
-        val mapping = req.data("type")
-    
-        val topic = req.task.split(":")(1) match {
-          
-          case "item" => "item"
-          
-          case "rule" => "rule"
-          
-          case _ => {
-            
-            val msg = Messages.TASK_IS_UNKNOWN(uid,req.task)
-            throw new Exception(msg)
-            
-          }
-        
-        }
-        
-        val builder = EBF.getBuilder(topic,mapping)
-        val indexer = new ElasticIndexer()
-    
-        indexer.create(index,mapping,builder)
-        indexer.close()
-      
-        val data = Map("uid" -> uid, "message" -> Messages.SEARCH_INDEX_CREATED(uid))
-        val response = new ServiceResponse(req.service,req.task,data,ResponseStatus.SUCCESS)	
-      
-        origin ! response
-      
-      } catch {
-        
-        case e:Exception => {
-          
-          log.error(e, e.getMessage())
-      
-          val data = Map("uid" -> uid, "message" -> e.getMessage())
-          val response = new ServiceResponse(req.service,req.task,data,ResponseStatus.FAILURE)	
-      
-          origin ! response
-          
-        }
-      
-      } finally {
-        
-        context.stop(self)
-
-      }
-    }
-    
-  }
-
-}
+class RuleIndexer extends BaseIndexer(Configuration)
